39a77ad78014188664771fa9e1274494417606aa
Ersan Karimi <ersankarimii311@gmail.com>
2024-11-08 20:34:18 +0800

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import antfu from "@antfu/eslint-config";
+
+export default antfu({
+  formatters: true,
+  vue: true,
+  stylistic: {
+    indent: 2,
+    quotes: "double",
+    semi: true,
+  },
+});
