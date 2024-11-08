3e2ec58881005d0dacb9a7dad805480e58cb851b
Ersan Karimi <ersankarimii311@gmail.com>
2024-11-08 21:14:14 +0800

    feat: configure shadcn module and add color mode options in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,15 +8,53 @@ export default defineNuxtConfig({
     "@nuxtjs/color-mode",
     "@vueuse/motion/nuxt",
   ],
+
+  /**
+   * Shadcn module options
+   * https://www.shadcn-vue.com/docs/installation/nuxt.html
+   */
   shadcn: {
     /**
      * Prefix for all the imported component
      */
-    prefix: "",
+    prefix: "Ui",
     /**
      * Directory that the component lives in.
      * @default "./components/ui"
      */
     componentDir: "./components/ui",
+  },
+
+  /**
+   * Color mode module options
+   * https://color-mode.nuxtjs.org/
+   */
+  colorMode: {
+    classSuffix: "",
+    preference: "system",
+    fallback: "dark",
+  },
+
+  ssr: false,
+
+  /**
+   * Nuxt to pre-render images
+   * https://image.nuxt.com/advanced/static-images
+   */
+  nitro: {
+    prerender: {
+      routes: [
+
+      ],
+    },
+  },
+
+  /**
+   * Nuxt Auto Import
+   */
+  imports: {
+    dirs: [
+      "lib",
+    ],
   },
 });
