01bb7fa50aa9147db28f1679aa9f469aa3e278cf
Ersan Karimi <ersankarimii311@gmail.com>
2024-11-08 21:14:23 +0800

    feat: add SEO configuration and update layout structure in app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
+<script setup lang="ts">
+const seoOptions: SeoConfig = {
+  title: "My Site | My site is awesome",
+  description: "My site is awesome",
+};
+
+useSeoMeta(generateSeoMeta(seoOptions));
+useHead(generateHeadConfig(seoOptions));
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
